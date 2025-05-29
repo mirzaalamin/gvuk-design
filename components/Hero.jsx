e22279c391d1e65fac85567398a8263935ee3fea
Mirza Alamin <mjapurbo2@gmail.com>
2025-05-29 22:06:57 +0600

    Fix hero image

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Hero = ({ title, description, imgUrl, titleColor, descriptionColor, logoUrl }) => {
     return (
         <div className="flex flex-col lg:flex-row py-8 lg:py-0 gap-10">
-            <div className="flex flex-1 flex-col lg:pt-0 gap-5 justify-center">
+            <div className="flex flex-1 flex-col lg:pt-0 gap-5 justify-center ">
                 <div className="flex flex-col gap-5 lg:py-[100px]">
                     {logoUrl && (
                         <Image
@@ -22,13 +22,14 @@ const Hero = ({ title, description, imgUrl, titleColor, descriptionColor, logoUr
                 </div>
             </div>
             {imgUrl && (
-                <div className="flex-1 hero-gradient">
+                <div className="flex-1 flex hero-gradient items-center justify-center">
                     <Image
                         src={imgUrl}
                         alt='Laser star'
                         quality={100}
                         width={800}
                         height={350}
+                        className='w-full h-full object-cover'
                         sizes="(max-width: 768px) 100vw, 800px"
                     />
                 </div>
